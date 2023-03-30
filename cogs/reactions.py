da0e65997412a0b66f0ccd7a0d530eca549f9726
Sercan Muhlacı <muhlacisercan@hotmail.com>
2023-03-30 23:28:25 +0300

    Working on reaction create

--- cogs/reactions.py
+++ cogs/reactions.py
@@ -20,10 +20,12 @@ class Reactions(commands.Cog):
     reactionGroup = SlashCommandGroup("reaction", "Reactions comes with roles")
 
     async def process_reaction(self, payload: RawReactionActionEvent, r_type=None) -> None:
-        reactions_db = TinyDB(DB_NAME).table("reactions")
+        reactions_db = TinyDB(DB_NAME)
+        reactions_table = reactions_db.table("reactions")
         data = Query()
 
-        reaction_data = reactions_db.get((data.message == payload.message_id) & (data.message.emoji_name == payload.emoji.name))
+        reaction_data = reactions_table.get((data.message_id == payload.message_id) & (data.message.emoji_name == payload.emoji.name))
+        reactions_db.close()
 
         if reaction_data is not None:
             guild = self.bot.get_guild(payload.guild_id)
@@ -52,12 +54,41 @@ class Reactions(commands.Cog):
         logging.info(payload)
         await self.process_reaction(payload, "remove")
 
-#    @discord.slash_command()
-    @reactionGroup.command()
-    async def reaction_add(self, interaction: discord.Interaction) -> None:
+    @reactionGroup.command(name='create', description="lorem ipsum")
+    async def reaction_create(self, ctx: discord.ext.commands.Context, channel_id, message_id, emoji, role_id):
+        reactions_db = TinyDB(DB_NAME)
+        reactions_table = reactions_db.table("reactions")
+        data = Query()
+
+        channel: discord.abc.Messageable = ctx.guild.get_channel(int(channel_id))
+        message: discord.Message = await channel.fetch_message(int(message_id))
+        if any(reaction.emoji == emoji for reaction in message.reactions):
+            logging.error("Target message is already has that reaction. Remove it first.")
+        else:
+            try:
+                await message.add_reaction(emoji)
+                pass
+            except discord.errors.ApplicationCommandInvokeError as e:
+                if isinstance(e.original, discord.errors.HTTPException):
+                    status_code = e.original.status
+                    error_code = e.original.code
+                    if status_code == 400 and error_code == 10014:
+                        logging.error("This emoji can not reachable from bot. "
+                                      "Does it exist or is it from another server?")
+
+
+        #if reactions_db.contains(data.message_id == message_id):
+    pass
+
+    @reactionGroup.command(name='remove', description="lorem ipsum")
+    async def reaction_remove(self, message_id, emoji):
         logging.info('hello world!')
         pass
 
+    @reactionGroup.command(name='clear', description="lorem ipsum")
+    async def reaction_clear(self, message_id):
+        logging.info('hello world!')
+        pass
 
 def setup(bot):
     bot.add_cog(Reactions(bot))
